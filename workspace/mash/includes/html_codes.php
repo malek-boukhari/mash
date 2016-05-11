667e61cfb04a7b3e867b2a11413dbf1bbc538854
Shady Smaoui <shadysmaoui@hotmail.fr>
2016-05-10 23:18:58 -0400

    html_codes.php:fuction footerCode(); css: main_footer

--- workspace/mash/includes/html_codes.php
+++ workspace/mash/includes/html_codes.php
@@ -1,4 +1,18 @@
 <?php
 //fuction header() we call everytime we have a new page
-//function footer() we call everytime we have a new page
+
+//Code for footer
+function footerCode(){
+	echo '
+		<footer id="main_footer">
+			<table>
+				<tr>
+					<td><a href="https://www.facebook.com/shady.smaoui">Ifah YouTube</a></td>
+					<td><a href="https://www.facebook.com/shady.smaoui">Ifah Facebook</a></td>
+					<td><a href="https://www.facebook.com/shady.smaoui">Ifah Google+</a></td>
+				</tr>
+			</table>
+		</footer>
+	';
+}
 ?>
