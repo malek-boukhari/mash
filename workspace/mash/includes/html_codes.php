2efd99e441260ea79314671ebb74b9d1eab73e51
Malek <godsent615@gmail.com>
2016-05-11 21:06:23 +0200

    pages.css,maincss.css:header center, colors changed (header and footer)

--- workspace/mash/includes/html_codes.php
+++ workspace/mash/includes/html_codes.php
@@ -4,16 +4,18 @@ function headerCode(){
 	echo'
 		<header id="main_header">
 			<h1><a href="index.php">Smoub</a></h1>	
-			<h2><a class="button" href="following.php">Following</a></h2>
-			<h2><a class="button" href="today.php">Today</a></h2>
-			<h2><a class="button" href="fashion.php">Fashion</a></h2>
-			<h2><a class="button" href="motors.php">Motors</a></h2>
-			<h2><a class="button" href="electronics.php">Electronis</a></h2>
-			<h2><a class="button" href="sporting.php">Sporting</a></h2>
-			<h2><a class="button" href="toysandhobbies.php">Today</a></h2>
-			<h2><a class="button" href="home.php">Home</a></h2>
-			<h2><a class="button" href="collectibles.php">Collectibles</a></h2>
-			<h2><a class="button" href="collectibles.php">Other</a></h2>
+			<div>
+				<h2><a class="button" href="following.php">Following</a></h2>
+				<h2><a class="button" href="today.php">Today</a></h2>
+				<h2><a class="button" href="fashion.php">Fashion</a></h2>
+				<h2><a class="button" href="motors.php">Motors</a></h2>
+				<h2><a class="button" href="electronics.php">Electronis</a></h2>
+				<h2><a class="button" href="sporting.php">Sporting</a></h2>
+				<h2><a class="button" href="toysandhobbies.php">Today</a></h2>
+				<h2><a class="button" href="home.php">Home</a></h2>
+				<h2><a class="button" href="collectibles.php">Collectibles</a></h2>
+				<h2><a class="button" href="collectibles.php">Other</a></h2>
+			</div>
 		</header>	
 	';
 }
