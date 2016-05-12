a64edd8bde7eb09f8ef5a801e34c9a075b53c6e4
Shady Smaoui <shadysmaoui@hotmail.fr>
2016-05-12 11:36:46 -0400

    fixhing css...delete superfluoustags

--- workspace/mash/includes/html_codes.php
+++ workspace/mash/includes/html_codes.php
@@ -1,30 +1,27 @@
 <?php
 //fuction header() we call everytime we have a new page
+//<h1><a href="index.php"><img src= "css/images/smob.jpeg"</a></h1>
 function headerCode(){
 	echo'
-		<header id="main_header">
-			<h1><a href="index.php"><img src= "css/images/smob.jpeg"</a></h1>
-			<br>
+			<div id="banner">
 				<div id="IDlogin">
 					<form align="right"><input type="text" placeholder="username"/>
 					<input type="text" placeholder="password"/>
 					<input type="submit" id ="button" value="login"/>
-					<p><a href="">Forgot your password?</a></p>
 				</div>
-			<div id="banner">
-				<h2><a class="button" href="following.php">Following</a></h2>
-				<h2><a class="button" href="today.php">Today</a></h2>
-				<h2><a class="button" href="fashion.php">Fashion</a></h2>
-				<h2><a class="button" href="motors.php">Motors</a></h2>
-				<h2><a class="button" href="electronics.php">Electronis</a></h2>
-				<h2><a class="button" href="sporting.php">Sporting</a></h2>
-				<h2><a class="button" href="toysandhobbies.php">Today</a></h2>
-				<h2><a class="button" href="home.php">Home</a></h2>
-				<h2><a class="button" href="collectibles.php">Collectibles</a></h2>
-				<h2><a class="button" href="collectibles.php">Other</a></h2>
+				<div id="menu">
+					<td><a class="button" href="following.php">Following</a></td>
+					<td><a class="button" href="today.php">Today</a></td>
+					<td><a class="button" href="fashion.php">Fashion</a></td>
+					<td><a class="button" href="motors.php">Motors</a></td>
+					<td><a class="button" href="electronics.php">Electronis</a></td>
+					<td><a class="button" href="sporting.php">Sporting</a></td>
+					<td><a class="button" href="toysandhobbies.php">Today</a></td>
+					<td><a class="button" href="home.php">Home</a></td>
+					<td><a class="button" href="collectibles.php">Collectibles</a></td>
+					<td><a class="button" href="collectibles.php">Other</a></td>
+				</div>
 			</div>
-
-		</header>	
 	';
 	
 	echo "
