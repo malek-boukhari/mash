edff6dabdfeb33675e1a0656fda5a620de85b56d
Shady Smaoui <shadysmaoui@hotmail.fr>
2016-05-14 02:20:34 -0400

    3h of work to fix connection to dataBase

--- workspace/mash/includes/createConnection.php
+++ workspace/mash/includes/createConnection.php
@@ -1,12 +1,10 @@
 <?php
 $conn_error = 'Could not connect.';
 $mysql_host = 'localhost';
-$mysql_user = 'root';//to change
-$mysql_pass = 'password';//to change
-$mysql_connect = @mysqli_connect($mysql_host, $mysql_user, $mysql_pass);
+$mysql_user = 'shady';
+$mysql_pass = '0000';
 $mysql_db = 'shady_dataBase';
-if(!@mysqli_connect($mysql_host, $mysql_user, $mysql_pass) || !@mysqli_select_db($mysql_connect, $mysql_db))
-{
-	die($conn_error);
-}
+$mysql_connect = @mysqli_connect($mysql_host, $mysql_user, $mysql_pass,$mysql_db) or die($conn_error);
+
+echo "Connection succeded, GoodGame Shady!";
 ?>
